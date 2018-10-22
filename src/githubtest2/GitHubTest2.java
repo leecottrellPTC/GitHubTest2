515606cec983aa52e84bdb1403d79f873cedeba3
ghk2300@gmail.com <>
2018-10-22 15:36:25 -0400

    Jonathon Lodge - added a LodgeWasHere() method

--- src/githubtest2/GitHubTest2.java
+++ src/githubtest2/GitHubTest2.java
@@ -24,4 +24,8 @@ public class GitHubTest2 {
         System.out.println("Woo Hoo");
         System.out.println("Woo Hoo again");
     }
+     
+     public static void LodgeWasHere(){
+         System.out.println("John was here...");
+     }
 }
