564e14ed306e2bfe4a95386eb96b9a87a451e5c8
cowdy71jake@gmail.com <>
2018-10-22 15:27:33 -0400

    I committed lolololol

--- src/githubtest2/GitHubTest2.java
+++ src/githubtest2/GitHubTest2.java
@@ -17,11 +17,16 @@ public class GitHubTest2 {
     public static void main(String[] args) {
         // TODO code application logic here
         Cottrell();
+        Cowden();
     }
     
      public static void Cottrell(){
         System.out.println("Lee Cottrell!!");
         System.out.println("Woo Hoo");
         System.out.println("Woo Hoo again");
+    }
+     
+    public static void Cowden(){
+        System.out.println("Cowden was here");
     }
 }
