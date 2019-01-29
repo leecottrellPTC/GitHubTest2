979c04fcaefa0e41ef7e702d2532f55de0f267e4
Lee Cottrell <44240965+leecottrellPTC@users.noreply.github.com>
2019-01-29 13:44:05 -0500

    Update GitHubTest2.java

--- src/githubtest2/GitHubTest2.java
+++ src/githubtest2/GitHubTest2.java
@@ -18,8 +18,6 @@ public class GitHubTest2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Cottrell();
-        DominicWasHere();
     }
     
      public static void Cottrell(){
@@ -28,21 +26,5 @@ public class GitHubTest2 {
         System.out.println("Woo Hoo again");
     }
      
-     public static void DominicWasHere(){
-         System.out.print("Steppin on the beach!");
-         Timer timer = new Timer();
-         timer.schedule(new TimerTask(){
-             public void run(){
-                 try{
-                    AudioInputStream audio = AudioSystem.getAudioInputStream(
-                        new File("./Grass_Beach.wav"));
-                    Clip clip = AudioSystem.getClip();
-                    clip.open(audio);
-                    clip.start();
-                } catch(Exception e){
-                    System.out.println("Error Playing Audio File");
-                }
-             }
-         }, 0, 15000);
-     }
+     
 }
