d58de964cfa9a69f6c53f738c9034e7cd991310d
avh1 <avh1@FSTU603018.pti.edu>
2019-02-01 09:24:39 -0500

    AUSTIN HALE
    Added CottrellWasHere() method
    
    Added infinite loop for no good reason

--- src/githubtest2/GitHubTest2.java
+++ src/githubtest2/GitHubTest2.java
@@ -27,6 +27,7 @@ public class GitHubTest2 {
     }
      
      public static void CottrellWasHere(){
+         //infinite loop
          for(int i = 0; i < 20; i++)
          {
              if(i == 19)
