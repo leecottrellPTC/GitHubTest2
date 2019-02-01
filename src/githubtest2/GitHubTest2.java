72ff451344ca5ccb7cfdd84d6e6cb334a3baf0df
alk31@students.ptcollege.edu <>
2019-02-01 10:11:20 -0500

    Alix Kramer public static void alixWasHere(){ System.out.println("Alix was here"); }

--- src/githubtest2/GitHubTest2.java
+++ src/githubtest2/GitHubTest2.java
@@ -26,5 +26,9 @@ public class GitHubTest2 {
         System.out.println("Woo Hoo again");
     }
      
+     public static void alixWasHere(){
+         System.out.println("Alix was here");
+     }
+     
      
 }
