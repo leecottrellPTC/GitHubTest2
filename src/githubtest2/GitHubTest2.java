af33155b80837ff73b9afda3c28fba010572f0de
swatcam1@gmail.com <>
2019-02-01 09:47:51 -0500

    My name is Cameron Young and I added a new function to the existing code.

--- src/githubtest2/GitHubTest2.java
+++ src/githubtest2/GitHubTest2.java
@@ -26,5 +26,7 @@ public class GitHubTest2 {
         System.out.println("Woo Hoo again");
     }
      
-     
+     public static void youngWasHere(){
+         System.out.println("Young was here");
+     }
 }
