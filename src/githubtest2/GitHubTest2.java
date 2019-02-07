d9b1894d938200ad03b2fa07d0d43936d171fb7d
jcp14@students.ptcollege.edu <>
2019-02-07 11:45:20 -0500

    Added method that says I was here

--- src/githubtest2/GitHubTest2.java
+++ src/githubtest2/GitHubTest2.java
@@ -26,5 +26,9 @@ public class GitHubTest2 {
         System.out.println("Woo Hoo again");
     }
      
-     
+     public static void joeyWasHere() {
+         
+         System.out.println("Joey was here");
+         
+     }
 }
