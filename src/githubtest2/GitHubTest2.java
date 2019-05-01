6873b7cd60fa4ea3a9c9a8d8010d039d2a1eb426
NR10 <NR10@FSTU208024.pti.edu>
2019-05-01 18:30:25 -0400

    Nate Rice, added function that prints a message about vandalism

--- src/githubtest2/GitHubTest2.java
+++ src/githubtest2/GitHubTest2.java
@@ -25,6 +25,8 @@ public class GitHubTest2 {
         System.out.println("Woo Hoo");
         System.out.println("Woo Hoo again");
     }
-     
+     public static void Rice(){
+         System.out.println("Modern day vandalism");
+     }
      
 }
