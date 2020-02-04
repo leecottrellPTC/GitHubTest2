61925a17a0e3dd9a12ad7c03c94f2be6bcaa7401
ClaytonTimmons <60448885+ClaytonTimmons@users.noreply.github.com>
2020-02-04 13:01:37 -0500

    Clayton Timmons added cottrellIsSmart

--- src/githubtest2/GitHubTest2.java
+++ src/githubtest2/GitHubTest2.java
@@ -26,5 +26,9 @@ public class GitHubTest2 {
         System.out.println("Woo Hoo again");
     }
      
+     public static void cottrellIsSmart(){
+         System.out.println("Cottrell is the smartest man alive.");
+     }
+     
      
 }
