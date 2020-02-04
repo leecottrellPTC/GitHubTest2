06f9df8e8332d86e423c092c759fedec66281068
OldManCapone <56334420+OldManCapone@users.noreply.github.com>
2020-02-04 15:58:46 -0500

    Angelo Capone -- Added a function that tells you I was there, and asks about a coin. To a Witcher.

--- src/githubtest2/GitHubTest2.java
+++ src/githubtest2/GitHubTest2.java
@@ -1,30 +1,27 @@
 package githubtest2;
-
 import java.io.File;
 import java.util.Timer;
 import java.util.TimerTask;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
-
 /**
  *
  * @author cottrell.lee
  */
-public class GitHubTest2 {
-
+public class GitHubTest2{
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[]args){
         // TODO code application logic here
     }
-    
-     public static void Cottrell(){
+    public static void Cottrell(){
         System.out.println("Lee Cottrell!!");
         System.out.println("Woo Hoo");
         System.out.println("Woo Hoo again");
     }
-     
-     
+    public static void  angeloWasHere(){
+        System.out.println("Angelo was here, did you toss a coin to your witcher?");
+    }
 }
