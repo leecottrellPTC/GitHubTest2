987077dc04c65c9d14b54862e3c061f0a98cc109
robloxguyed@gmail.com <>
2020-02-04 16:06:43 -0500

    Bednar Was Here method added - Jeremy Bednar

--- src/githubtest2/GitHubTest2.java
+++ src/githubtest2/GitHubTest2.java
@@ -25,6 +25,8 @@ public class GitHubTest2 {
         System.out.println("Woo Hoo");
         System.out.println("Woo Hoo again");
     }
-     
+     public static void BednarWasHere(){
+         System.out.println("Bednar was here");
+     }
      
 }
